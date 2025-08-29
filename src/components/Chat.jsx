519516b064cfc1f7d3636fd7084f088731a8ada6
Sameque Sant' ana <samequesantana@gmail.com>
2025-08-29 13:49:07 -0300

    deploy  vercel-011

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,14 +3,12 @@ import UserList from "./UserList";
 import Message from "./Message";
 import { jwtDecode } from "jwt-decode";
 import { createClient } from "@supabase/supabase-js";
-
+// import process from "process";
 import { Menu } from "lucide-react";
 
-console.log("Supabase URL:", import.meta.env.DATABASE_URL);
-console.log("Supabase Key:", import.meta.env.DATABASE_URL);
+console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("Supabase Key:", import.meta.env.VITE_SUPABASE_KEY);
 console.log(import.meta.env);
-console.log(import.meta);
-// console.log('DATABASE_URL', process.env.DATABASE_URL)
 
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,

e7f01690399192afae470bf6faa016bb90d27672
Sameque Sant' ana <samequesantana@gmail.com>
2025-08-29 00:30:26 -0300

    deploy  vercel-009

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -9,6 +9,7 @@ import { Menu } from "lucide-react";
 console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_URL);
 console.log("Supabase Key:", import.meta.env.VITE_SUPABASE_KEY);
 console.log(import.meta.env);
+console.log(import.meta);
 
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,

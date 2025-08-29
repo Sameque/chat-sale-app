33f7a42536d7c7e706c060690a7a079c04168ef9
Sameque Sant' ana <samequesantana@gmail.com>
2025-08-28 22:58:12 -0300

    deploy  vercel-003

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from "react";
 import Login from "./components/Login";
 import Chat from "./components/Chat";
-import { env } from 'meta';
 
 export default function App() {
   
   console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_URL);
   console.log("Supabase Key:", import.meta.env.VITE_SUPABASE_KEY);
-  console.log("Environment:", env);
-  
   
   const [token, setToken] = useState(sessionStorage.getItem("chat-app-token") || null);
 
